717333691fa60801392d7f722871b83bd01b8bd5
huyenminhdo <dominhhuyen477@gmail.com>
2023-12-02 14:40:46 +0700

    update

--- admin/index.php
+++ admin/index.php
@@ -26,47 +26,48 @@
     <script src="../assets/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="./css/styleadmin.css">        
 </head>
-<body background-color: #ffffcc>
+<body class="admin">
     <h1>Trang quản trị Admin</h1>
     <div class="row button">
-    <div class="col">
-        <button>
-            <a href="index.php?categories">All categories</a>
-        </button>
+        <div class="col">
+            <button>
+                <a href="index.php?categories">All categories</a>
+            </button>
             
-        <button>
-            <a href="index.php?products">All products</a>
-        </button>
-        <button>
-            <a href="index.php?news">All news</a>
-        </button>
+            <button>
+                <a href="index.php?products">All products</a>
+            </button>
+            <button>
+                <a href="index.php?news">All news</a>
+            </button>
 
-        <button>
-            <a href="#">All order</a>
-        </button>
+            <button>
+                <a href="#">All order</a>
+            </button>
 
-        <button>
-            <a href="#">List users</a>
-        </button>
+            <button>
+                <a href="#">List users</a>
+            </button>
 
-        <button>
-            <a href="index.php?logout">Log out</a>
-        </button>
-    </div>
-    </div>
+            <button>
+                <a href="index.php?logout">Log out</a>
+            </button>
+        </div>
+        </div>
 
-    <div class="contaitner-fluid">
-    <?php 
-        if(isset($_GET['products'])) {
-            include ('./quanlysanpham/product.php');
-        }
-        elseif(isset($_GET['categories'])) {
-            include ('./quanlydanhmuc/category.php');
-        }
-        elseif(isset($_GET['news'])) {
-            include ('./quanlytintuc/news.php');
-        }
-    ?>
+        <div class="contaitner-fluid">
+            <?php 
+            if(isset($_GET['products'])) {
+                include ('./quanlysanpham/product.php');
+            }
+            elseif(isset($_GET['categories'])) {
+                include ('./quanlydanhmuc/category.php');
+            }
+            elseif(isset($_GET['news'])) {
+                include ('./quanlytintuc/news.php');
+            }
+            ?>
+        </div>
     </div>
 </body>
 </html>
