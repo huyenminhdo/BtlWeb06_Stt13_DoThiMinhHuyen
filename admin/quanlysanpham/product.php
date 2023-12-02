14ce6e3e1d3c547ad833d7455f8ad443d861991b
huyenminhdo <dominhhuyen477@gmail.com>
2023-12-02 14:48:17 +0700

    update

--- admin/quanlysanpham/product.php
+++ admin/quanlysanpham/product.php
@@ -38,7 +38,7 @@
 
         if(mysqli_query($conn, $sql_insert)) {
             echo "<script>alert('Đã thêm sản phẩm thành công')</script>";
-            echo "<script>window.open('./index.php?products','_self')</script>";
+            echo "<script>window.open('./dashboard.php?products','_self')</script>";
         } 
         else {
             echo "Error: " . $sql_insert . "<br>" . mysqli_error($conn);
@@ -94,7 +94,7 @@
             <h1>SẢN PHẨM</h1>
             <div class="row">
                 <div class="col-12">
-                    <form class="form-check-inline " action="./index.php?products" method="post">
+                    <form class="form-check-inline " action="./dashboard.php?products" method="post">
                             <input class="form-control" style="width: 300px;" type="text" name="txt_search" id="" placeholder="Tìm kiếm theo mã sản phẩm....">
                             <br>
                             <input class="btn btn-success" type="submit" value="Tìm kiếm" name="btn_search">
@@ -126,7 +126,7 @@
                             <th>Chọn</th>
                         </tr>
                         <!-- Form bao trọn table -->
-                        <form action="./index.php?products" method="post">
+                        <form action="./dashboard.php?products" method="post">
                             <input type="submit" value="Xóa theo lựa chọn" name = "delete_check" class="btn btn-info"> 
                             <?php
                                 $sql = "";
@@ -176,7 +176,7 @@
                                                         </svg> 
                                                     </a> 
                                                     </td>";
-                                            echo "<td><a class='btn btn-danger' href='./index.php?products=delete&id=".$row["product_id"]."'>
+                                            echo "<td><a class='btn btn-danger' href='./dashboard.php?products=delete&id=".$row["product_id"]."'>
                                                         <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-trash3-fill' viewBox='0 0 16 16'>
                                                             <path d='M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z'/>
                                                         </svg>
@@ -190,7 +190,7 @@
                                 }
                                 else {
                                     echo "<script>alert('Không tìm thấy mã sản phẩm này')</script>";
-                                    echo "<script>window.open('./index.php?products','_self')</script>";
+                                    echo "<script>window.open('./dashboard.php?products','_self')</script>";
                                 }
                         
                             ?>
@@ -201,7 +201,7 @@
 
             <h2>Thêm sản phẩm</h2>
             <!-- form -->
-            <form action="./index.php?products" method="post" enctype="multipart/form-data">
+            <form action="./dashboard.php?products" method="post" enctype="multipart/form-data">
                 Chọn danh mục sản phẩm:
                 <select class="form-control" name="cate" id="">
                     <?php
